a0c04e61f74ca81c75c28f7c3f4348b9617770f8
Jan Siemiński <jan.sieminski@websensa.com>
2020-12-23 09:52:01 +0100

    Add solution to #23

--- aoc23.py
+++ aoc23.py
@@ -1,50 +1,79 @@
+
+def run_task(lines, which_one=1):
+    cups = list(map(int, lines[0]))
+    if which_one == 2:
+        cups = cups + list(range(max(cups)+1, 1000001))
+
+    linked_list = {}
+    value_to_pointer = {}
+    for index, elem in enumerate(cups):
+        linked_list[index] = (elem, index+1 if index+1 < len(cups) else 0)
+        value_to_pointer[elem] = index
+
+    run_moves(10000000 if which_one == 2 else 100, linked_list, value_to_pointer)
+
+    if which_one == 1:
+        result = []
+        next_pointer = value_to_pointer[1]
+        while True:
+            result.append(linked_list[next_pointer][0])
+            next_pointer = linked_list[next_pointer][1]
+
+            if next_pointer == value_to_pointer[1]:
+                break
+
+        print('first_task', ''.join(map(str, result[1:])))
+    else:
+        a = linked_list[linked_list[value_to_pointer[1]][1]]
+        b = linked_list[a[1]]
+        print('second_task', a[0], b[0], a[0] * b[0])
+
+
+def run_moves(moves_number, linked_list, value_to_pointer):
+    max_value = max(value_to_pointer)
+    new_addr = len(linked_list) + 1
+
+    current_cup_pointer = 0
+
+    for i in range(moves_number):
+        picked_cups = [-1]*3
+
+        current_value, next_pointer = linked_list[current_cup_pointer]
+        for i in range(3):
+            current_pointer = next_pointer
+            value, next_pointer = linked_list[current_pointer]
+            del linked_list[current_pointer]
+            del value_to_pointer[value]
+            picked_cups[i] = value
+
+        linked_list[current_cup_pointer] = (current_value, next_pointer)
+
+        destination_value = current_value - 1
+        while True:
+            if destination_value < 1:
+                destination_value = max_value
+            if destination_value in picked_cups:
+                destination_value -= 1
+            else:
+                break
+
+        destination_pointer = value_to_pointer[destination_value]
+
+        for i in range(2, -1, -1):
+            linked_list[new_addr] = (
+                picked_cups[i], linked_list[destination_pointer][1])
+            linked_list[destination_pointer] = (
+                linked_list[destination_pointer][0], new_addr)
+            value_to_pointer[picked_cups[i]] = new_addr
+            new_addr += 1
+
+        current_cup_pointer = linked_list[value_to_pointer[current_value]][1]
+
+
 if __name__ == '__main__':
 
     with open('./23.input', 'r') as f:
         lines = f.read().splitlines()
 
-    cups = list(map(int, lines[0]))
-    print('cups', cups)
-    current_cup = cups[0]
-    current_cup_index = cups.index(current_cup)
-
-    cups = cups + list(range(max(cups)+1, 1000001))
-
-    for i in range(10000000):
-        if i % 100000 == 0:
-            print(i)
-
-        picked_cups = cups[current_cup_index+1:current_cup_index+4]
-        cups = cups[:current_cup_index+1] + cups[current_cup_index+4:]
-
-        if len(picked_cups) < 3:
-            left = 3 - len(picked_cups)
-            picked_cups = picked_cups + cups[:left]
-            cups = cups[left:]
-
-        # print(picked_cups)
-        # print(cups)
-
-        destination_cup_index = None
-        destination_value = current_cup - 1
-        while destination_cup_index == None:
-            if destination_value in cups:
-                destination_cup_index = cups.index(destination_value)
-            else:
-                destination_value -= 1
-                if destination_value < min(cups):
-                    destination_value = max(cups)
-
-        # print('destination_cup_index', destination_cup_index)
-
-        cups = cups[:destination_cup_index+1] + picked_cups + cups[destination_cup_index+1:]
-        # print(cups)
-
-        current_cup_index = (cups.index(current_cup) + 1) % len(cups)
-        current_cup = cups[current_cup_index]
-
-        # print('current_cup_index', current_cup_index)
-        # print('current_cup', current_cup)
-
-    ind = cups.index(1)
-    print(cups[ind+1], cups[ind+2], cups[ind+1]*cups[ind+2])
+    run_task(lines, 1)
+    run_task(lines, 2)
